f63c07b02d240d6af0da5938b3b27ee80671306a
Alison Doyle <S00233102@atu.ie>
2026-04-10 17:37:24 +0100

    feature: created article handler service

--- src/app/services/article-handler/article-handler.ts
+++ src/app/services/article-handler/article-handler.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleHandler {
+  // properties
   private _apiUrl: string = environment.API_URL;
 
-  public CreateArticle() {
+  // constructors
+  constructor(private _httpClient: HttpClient) {
 
+  }
+
+  // methods
+  public CreateArticle(article: { title: string, content: string }) {
+    return this._httpClient.post(this._apiUrl + "/article", article)
+      .pipe(tap((res) => {
+        console.log(res);
+      }))
   }
 
   public ReadArticle(articleId: number) {
